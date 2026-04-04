02217aaf0b188c41a08eed591340e61ec4e1a323
crisca645 <crisca645@gmail.com>
2026-04-03 23:47:31 -0500

    Delete ms-security directory

--- ms-security/src/main/java/com/ccrr/ms_security/Repositories/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ccrr.ms_security.Repositories;
-
-import com.ccrr.ms_security.Models.Role;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface RoleRepository extends MongoRepository<Role,String> {
-}
