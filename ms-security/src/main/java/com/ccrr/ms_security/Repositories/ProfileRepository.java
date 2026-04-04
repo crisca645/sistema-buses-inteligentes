d323ba344f8da39bc8d4dcb36c546f7f1f433618
crisca645 <crisca645@gmail.com>
2026-04-03 23:44:28 -0500

    Delete ms-security directory

--- ms-security/src/main/java/com/ccrr/ms_security/Repositories/ProfileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ccrr.ms_security.Repositories;
-
-import com.ccrr.ms_security.Models.Profile;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProfileRepository extends MongoRepository<Profile,String> {
-}
